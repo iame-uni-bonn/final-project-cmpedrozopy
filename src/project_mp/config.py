73acfd33301f0a983fc181cd98c73144ed8ed720
moisespedrozo <moises.pedrozo@uni-bonn.de>
2025-01-27 22:04:32 +0100

    Check pytask for first function.

--- src/project_mp/config.py
+++ src/project_mp/config.py
@@ -2,12 +2,11 @@
 
 from pathlib import Path
 
-SRC = Path(__file__).parent.resolve()
-ROOT = SRC.joinpath("..", "..").resolve()
+PROJECT = Path(__file__).parent.resolve()
+SRC = Path(__file__).parent.parent.resolve()
+ROOT = PROJECT.joinpath("..", "..").resolve()
 
 BLD = ROOT.joinpath("bld").resolve()
 
 
 DOCUMENTS = ROOT.joinpath("documents").resolve()
-
-TEMPLATE_GROUPS = ["marital_status", "highest_qualification"]
