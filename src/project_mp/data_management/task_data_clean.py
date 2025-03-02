f7c948aba2c78cf8a6fe242bef75f0d6b0f2cb69
Moises Pedrozo <moisespedrozo@Moisess-MacBook-Air.local>
2025-03-02 19:53:34 -0300

    Modified task shape files to csv.

--- src/project_mp/data_management/task_data_clean.py
+++ src/project_mp/data_management/task_data_clean.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from project_mp.config import BLD, SRC
 from project_mp.data_management.process_distance_data import (
-    process_distance_data,
+    process_distance_data, shape_to_csv
 )
 
 
@@ -16,3 +16,14 @@ def task_process_distance_data(
     process_distance_data(data, produces)
     processed_data = pd.read_csv(produces, encoding="latin1")
     processed_data.to_pickle(produces)
+
+def task_shape_to_csv(
+    script=SRC / "data_management" / "process_distance_data.py",
+    data=SRC / "data" ,
+    produces=BLD / "data" / "distance_to_missions.pickle",
+):
+    """Clean the distance data set."""
+    shape_to_csv(data, produces)
+    processed_data = pd.read_csv(produces, encoding="latin1")
+    processed_data.to_pickle(produces)
+    return processed_data
